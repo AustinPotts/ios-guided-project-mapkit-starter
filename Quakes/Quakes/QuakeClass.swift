67fe4c5140efc83c8f6911f5d87e5e2741bf3028
AustinPotts <pottsevolvements@gmail.com>
2019-12-08 12:30:24 -0500

    Ready to Test Model with Unit Test

---
+++ Quakes/Quakes/QuakeClass.swift
@@ -0,0 +1,19 @@
+//
+//  QuakeClass.swift
+//  Quakes
+//
+//  Created by Austin Potts on 12/8/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+
+// MKAnnotation requires we subclass from NSObject
+// Allows us to show data on a map
+
+class Quake: NSObject, Decodable {
+    
+    
+    
+}
